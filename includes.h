519d21a4106e73c69ee631d90298021152fb969e
Diego (pc fisso) <diego.conti@unimib.it>
2020-03-11 10:33:56 +0100

    added missing include to repository

---
+++ includes.h
@@ -0,0 +1,50 @@
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+#include <vector>
+#include <list>
+#include <map>
+#include <utility>
+#include <set>
+
+#include <string>
+
+#include <memory>
+
+#include <algorithm>
+#include <numeric>
+#include <limits>
+
+#include <stdexcept>
+#include <cassert>
+
+
+using std::ostream;
+using std::istream;
+using std::ofstream;
+using std::ifstream;
+using std::cout;
+using std::endl;
+using std::stringstream;
+
+using std::vector;
+using std::list;
+using std::map;
+using std::pair;
+using std::make_pair;
+using std::set;
+
+using std::string;
+using namespace std::string_literals;
+
+using std::unique_ptr;
+using std::make_unique;
+
+using std::numeric_limits;
+using std::swap;
+using std::move;
+#endif
